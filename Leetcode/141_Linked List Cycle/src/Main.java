436ac3429a8dd65eb1f4ee05de0ac980599c55da
emma418 <105550943+emma418@users.noreply.github.com>
2024-02-23 23:58:14 -0500

    Add files via upload

---
+++ Leetcode/141_Linked List Cycle/src/Main.java
@@ -0,0 +1,34 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        // Press Opt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+
+        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
+        for (int i = 1; i <= 5; i++) {
+
+            // Press Ctrl+D to start debugging your code. We have set one breakpoint
+            // for you, but you can always add more by pressing Cmd+F8.
+            System.out.println("i = " + i);
+        }
+    }
+    public boolean hasCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while(fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (slow == fast) return true;
+        }
+        return false;
+    }
+    class ListNode {
+      int val;
+     ListNode next;
+      ListNode(int x) {val = x;
+          next = null;
+      }
+  }
+}
