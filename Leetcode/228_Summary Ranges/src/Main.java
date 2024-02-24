305db4cc96675483a0d78a649e4230f61335f0c6
emma418 <105550943+emma418@users.noreply.github.com>
2024-02-23 23:35:37 -0500

    Add files via upload

--- Leetcode/228_Summary Ranges/src/Main.java
+++ Leetcode/228_Summary Ranges/src/Main.java
@@ -10,9 +10,9 @@ public class Main {
     }
     public static List<String> summaryRanges(int[] nums) {
         List<String> result = new ArrayList<>();
-        if (nums.length == 0) return result; // Handle empty array
+        if (nums.length == 0) return result;
         if (nums.length == 1) {
-            result.add(String.valueOf(nums[0])); // Handle single-element array
+            result.add(nums[0] + "");
             return result;
         }
         for (int i = 0; i < nums.length; i++){
